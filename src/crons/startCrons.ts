2607e2809e8428d7460f6897ab0d5ea16c44c701
Рома <84147465+xretic@users.noreply.github.com>
2023-03-27 08:29:44 +0300

    Update startCrons.ts

--- src/crons/startCrons.ts
+++ src/crons/startCrons.ts
@@ -1,5 +1,7 @@
+import endDateCheck from "./tasks/endDateCheck";
 import notificationUsers from "./tasks/notificationUsers";
 
 export default async function (): Promise<void> {
 	await notificationUsers();
+	await endDateCheck();
 }
